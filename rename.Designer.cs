9ff6eb33c5327bab6bbeff1329f516fd1018db1c
frustreated <wangdingqian@126.com>
2019-11-29 09:17:45 +0800

    1st_upload

---
+++ rename.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace rename
+{
+    partial class rename
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(rename));
+            this.button_OK = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox_Rname = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_Dir = new System.Windows.Forms.TextBox();
+            this.textBox_1 = new System.Windows.Forms.TextBox();
+            this.textBox_2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_OK
+            // 
+            this.button_OK.Location = new System.Drawing.Point(168, 142);
+            this.button_OK.Name = "button_OK";
+            this.button_OK.Size = new System.Drawing.Size(100, 27);
+            this.button_OK.TabIndex = 0;
+            this.button_OK.Text = "确定";
+            this.button_OK.UseVisualStyleBackColor = true;
+            this.button_OK.Click += new System.EventHandler(this.button_OK_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "重命名";
+            // 
+            // comboBox_Rname
+            // 
+            this.comboBox_Rname.FormattingEnabled = true;
+            this.comboBox_Rname.Location = new System.Drawing.Point(85, 100);
+            this.comboBox_Rname.Name = "comboBox_Rname";
+            this.comboBox_Rname.Size = new System.Drawing.Size(282, 20);
+            this.comboBox_Rname.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "文件夹";
+            // 
+            // textBox_Dir
+            // 
+            this.textBox_Dir.Location = new System.Drawing.Point(85, 22);
+            this.textBox_Dir.Name = "textBox_Dir";
+            this.textBox_Dir.Size = new System.Drawing.Size(282, 21);
+            this.textBox_Dir.TabIndex = 4;
+            // 
+            // textBox_1
+            // 
+            this.textBox_1.Location = new System.Drawing.Point(85, 62);
+            this.textBox_1.Name = "textBox_1";
+            this.textBox_1.Size = new System.Drawing.Size(123, 21);
+            this.textBox_1.TabIndex = 5;
+            // 
+            // textBox_2
+            // 
+            this.textBox_2.Location = new System.Drawing.Point(244, 62);
+            this.textBox_2.Name = "textBox_2";
+            this.textBox_2.Size = new System.Drawing.Size(123, 21);
+            this.textBox_2.TabIndex = 6;
+            // 
+            // rename
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 183);
+            this.Controls.Add(this.textBox_2);
+            this.Controls.Add(this.textBox_1);
+            this.Controls.Add(this.textBox_Dir);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox_Rname);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_OK);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "rename";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "rename";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_OK;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox_Rname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_Dir;
+        private System.Windows.Forms.TextBox textBox_1;
+        private System.Windows.Forms.TextBox textBox_2;
+    }
+}
+
---
+++ rename.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rename
+{
+    public partial class rename : Form
+    {
+        public rename()
+        {
+            InitializeComponent();
+            comboBox_Rname.Items.Add("替换");
+            comboBox_Rname.Items.Add("base64");
+            comboBox_Rname.Items.Add("base64-bak");
+            comboBox_Rname.SelectedIndex = 0;
+        }
+
+        private void button_OK_Click(object sender, EventArgs e)
+        {
+            var choice = comboBox_Rname.Text;
+            if(choice== "替换")
+            {
+                var dir = textBox_Dir.Text;
+                var txt_1 = textBox_1.Text;
+                var txt_2 = textBox_2.Text;
+                var files = System.IO.Directory.GetFiles(dir, "*.*");
+                for (int i = 0; i < files.Length; i++)
+                {
+                    var fileName = System.IO.Path.GetFileName(files[i]);
+                    var endName = Path.GetDirectoryName(files[i]) + @"\" + fileName.Replace(txt_1, txt_2);
+                    File.Move(files[i], endName);
+                }
+            }
+            if(choice == "base64")
+            {
+                var dir = textBox_Dir.Text;
+                var files = System.IO.Directory.GetFiles(dir, "*.*");
+                for (int i = 0; i < files.Length; i++)
+                {
+                    var fileName = System.IO.Path.GetFileName(files[i]);
+                    byte[] b = System.Text.Encoding.Default.GetBytes(fileName);
+                    var endName = Path.GetDirectoryName(files[i]) + @"\" + Convert.ToBase64String(b);
+                    File.Move(files[i], endName);
+                }
+            }
+            if (choice == "base64-bak")
+            {
+                var dir = textBox_Dir.Text;
+                var files = System.IO.Directory.GetFiles(dir, "*.*");
+                for (int i = 0; i < files.Length; i++)
+                {
+                    var fileName = System.IO.Path.GetFileName(files[i]);
+                    byte[] b = Convert.FromBase64String(fileName);
+                    var endName = Path.GetDirectoryName(files[i]) + @"\" + System.Text.Encoding.Default.GetString(b);
+                    File.Move(files[i], endName);
+                }
+            }
+
+        }
+    }
+}
